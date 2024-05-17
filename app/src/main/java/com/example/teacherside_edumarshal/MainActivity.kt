9b1867196abaac36fd73d55ac2e0d730e2f5e882
params19 <paramsrivastava5@gmail.com>
2024-05-17 22:44:28 +0530

    login page

---
+++ app/src/main/java/com/example/teacherside_edumarshal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.teacherside_edumarshal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
